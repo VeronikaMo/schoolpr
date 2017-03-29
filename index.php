2b06b6c694bbfc0e5ae6586d899bbc37909d1e0e
VeronikaMo <v.motylova@gmail.com>
2017-03-29 09:45:30 +0200

    container

--- index.php
+++ index.php
@@ -17,6 +17,67 @@
 
     <div class="container">
         <div class="row">
+            <div class="col-md-3">
+                <div class="grey-box-icon b1">
+                    <h4>Kontrola známok</h4>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent tempus eleifend risus ut congue
+							eset nec lacus elit dor broma.
+                    </p>
+                    <p>
+                        <a href="#">
+                            <em>Viac</em>
+                        </a>
+                    </p>
+                </div><!--grey box -->
+            </div><!--/span3-->
+            <div class="col-md-3">
+                <div class="grey-box-icon b2">
+                    <h4>Kedy sú prázdniny?</h4>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent tempus eleifend risus ut congue
+							eset nec lacus elit dor broma.
+                    </p>
+                    <p>
+                        <a href="#">
+                            <em>Viac</em>
+                        </a>
+                    </p>
+                </div><!--grey box -->
+            </div><!--/span3-->
+            <div class="col-md-3">
+                <div class="grey-box-icon b3">
+                    <h4>Zajtra nás čaká</h4>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent tempus eleifend risus ut congue
+							eset nec lacus elit dor broma.
+                    </p>
+                    <p>
+                        <a href="#">
+                            <em>Viac</em>
+                        </a>
+                    </p>
+                </div><!--grey box -->
+            </div><!--/span3-->
+            <div class="col-md-3">
+                <div class="grey-box-icon b4">
+                    <h4>Domáce úlohy</h4>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent tempus eleifend risus ut congue
+							eset nec lacus elit dor broma.
+                    </p>
+                    <p>
+                        <a href="#">
+                            <em>Viac</em>
+                        </a>
+                    </p>
+                </div><!--grey box -->
+            </div><!--/span3-->
+        </div>
+    </div>
+    
+    <div class="container">
+        <div class="row">
             <div class="col-sm-4">
                 <h3>Rozvrh hodín</h3>
                 <p>-obsah-</p>
