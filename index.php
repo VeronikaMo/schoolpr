c464c4d80fe2e32e1a6e004a89645d9b7150cbcd
VeronikaMo <v.motylova@gmail.com>
2017-04-05 23:37:10 +0200

    update

--- index.php
+++ index.php
@@ -1,103 +1,163 @@
 <!DOCTYPE html>
 <html lang="sk">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-                <meta name="description" content="">
-                <title>  Škola</title>
-    <title>Bootstrap Example</title>
+    <title>School</title>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <style>
+    /* Remove the navbar's default rounded borders and increase the bottom margin */
+    .navbar {
+      margin-bottom: 50px;
+      border-radius: 0;
+    }
+
+    /* Remove the jumbotron's default bottom margin */
+     .jumbotron {
+      margin-bottom: 0;
+    }
+
+    /* Add a gray background color and some padding to the footer */
+    footer {
+      background-color: #f2f2f2;
+      padding: 25px;
+    }
+    </style>
 </head>
 <body>
 
-    <div class="jumbotron text-center">
-        <h1>ŠKOLA</h1>
-        <p>Sleduj svoje známky, najbližšie písomky, rozvrh a iné aktivity na škole</p>
+    <div class="jumbotron">
+        <div class="container text-center">
+            <img src="skola.gif" style="width:100%" />
+            <h1>Hura do skoly!</h1>
+        </div>
     </div>
+
+    <nav class="navbar navbar-inverse">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+
+            </div>
+            <div class="collapse navbar-collapse" id="myNavbar">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="#">O skole</a>
+                    </li>
+                    <li>
+                        <a href="#">Historia</a>
+                    </li>
+                    <li>
+                        <a href="#">Stores</a>
+                    </li>
+                    <li>
+                        <a href="#">Contact</a>
+                    </li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a href="#">
+                            <span class="glyphicon glyphicon-user"></span>Prihlasenie
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
 
     <div class="container">
         <div class="row">
-            <div class="col-md-3">
-                <div class="grey-box-icon b1">
-                    <h4>Kontrola známok</h4>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent tempus eleifend risus ut congue
-							eset nec lacus elit dor broma.
-                    </p>
-                    <p>
-                        <a href="#">
-                            <em>Viac</em>
-                        </a>
-                    </p>
-                </div><!--grey box -->
-            </div><!--/span3-->
-            <div class="col-md-3">
-                <div class="grey-box-icon b2">
-                    <h4>Kedy sú prázdniny?</h4>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent tempus eleifend risus ut congue
-							eset nec lacus elit dor broma.
-                    </p>
-                    <p>
-                        <a href="#">
-                            <em>Viac</em>
-                        </a>
-                    </p>
-                </div><!--grey box -->
-            </div><!--/span3-->
-            <div class="col-md-3">
-                <div class="grey-box-icon b3">
-                    <h4>Zajtra nás čaká</h4>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent tempus eleifend risus ut congue
-							eset nec lacus elit dor broma.
-                    </p>
-                    <p>
-                        <a href="#">
-                            <em>Viac</em>
-                        </a>
-                    </p>
-                </div><!--grey box -->
-            </div><!--/span3-->
-            <div class="col-md-3">
-                <div class="grey-box-icon b4">
-                    <h4>Domáce úlohy</h4>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent tempus eleifend risus ut congue
-							eset nec lacus elit dor broma.
-                    </p>
-                    <p>
-                        <a href="#">
-                            <em>Viac</em>
-                        </a>
-                    </p>
-                </div><!--grey box -->
-            </div><!--/span3-->
+            <div class="col-sm-4">
+                <div class="panel panel-primary" align="center">
+                    <div class="panel-heading">
+                        <a href="#">Rozvrh hodin</a>
+                    </div>
+                    <div class="panel-body">
+                        <img src="rozvrh.jpg" class="img-responsive" style="width:100%" alt="Image" />
+                    </div>
+                    <div class="panel-footer">Kedy, co a s kym?</div>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="panel panel-danger" align="center">
+                    <div class="panel-heading">
+                        <a href="#">Znamky</a>
+                    </div>
+                    <div class="panel-body">
+                        <img src="znamky.jpg" class="img-responsive" style="width:100%" alt="Image" />
+                    </div>
+                    <div class="panel-footer">Ako sa mi dari?</div>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="panel panel-success" align="center">
+                    <div class="panel-heading">
+                        <a href="#">Domace ulohy</a>
+                    </div>
+                    <div class="panel-body">
+                        <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image" />
+                    </div>
+                    <div class="panel-footer">Co mam urobit, aby som bol(a) o krok vpred?</div>
+                </div>
+            </div>
         </div>
     </div>
-    
+    <br />
+
     <div class="container">
         <div class="row">
             <div class="col-sm-4">
-                <h3>Rozvrh hodín</h3>
-                <p>-obsah-</p>
-                <p>-obsah frfrfrfrfrf-</p>
+                <div class="panel panel-primary" align="center">
+                    <div class="panel-heading">
+                        <a href="#">Sportove sutaze</a>
+                    </div>
+                    <div class="panel-body">
+                        <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image" />
+                    </div>
+                    <div class="panel-footer">A co tak si zmerat sily?</div>
+                </div>
             </div>
             <div class="col-sm-4">
-                <h3>Hodnotenia a úlohy</h3>
-                <p>-obsah-</p>
-                <p>-obsah-</p>
+                <div class="panel panel-primary" align="center">
+                    <div class="panel-heading">
+                        <a href="#">Vedomostne sutaze</a>
+                    </div>
+                    <div class="panel-body">
+                        <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image" />
+                    </div>
+                    <div class="panel-footer">Ci skor prevetrat mozgove zavity?</div>
+                </div>
             </div>
             <div class="col-sm-4">
-                <h3>Aktivity a súťaže</h3>
-                <p>-obsah-</p>
-                <p>-obsah-</p>
+                <div class="panel panel-primary" align="center">
+                    <div class="panel-heading">
+                        <a href="#">Zaujimavosti</a>
+                    </div>
+                    <div class="panel-body">
+                        <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image" />
+                    </div>
+                    <div class="panel-footer">Co nove sa deje?</div>
+                </div>
             </div>
         </div>
     </div>
+    <br />
+    <br />
+
+    <footer class="container-fluid text-center">
+        <p>Online Store Copyright</p>
+        <form class="form-inline">
+            Get deals:
+            <input type="email" class="form-control" size="50" placeholder="Email Address" />
+            <button type="button" class="btn btn-danger">Sign Up</button>
+        </form>
+    </footer>
 
 </body>
 </html>
